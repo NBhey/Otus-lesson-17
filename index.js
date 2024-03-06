5d9f96d806ed93212f9bb6da8eac2366a4c2ae58
Oganes <og.marckaryan@yandex.ru>
2024-03-06 14:39:00 +0300

    get user data (geo,temp,img)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 (async function () {
     const paragraphe = document.querySelector('div');
     const userCity = document.createElement('p');
+    userCity.classList.add('cityUser');
     const temp = document.createElement('p')
-    
+    temp.classList.add('tempUser');
+    const image = document.createElement('img')
+    image.classList.add('imageUser');
+
     async function getCity() {
         const response = await fetch(
           `https://get.geojs.io/v1/ip/geo.json`
@@ -15,20 +19,27 @@
         const dataCity = await getCity();
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=fd185eec875c277bc16bccdb3629b6af`);
         const data = await response.json();
+        console.log(data)
         return data['main'].temp;
     }
     getTemperature();
-      
+    async function imageGet(){
+      const dataCity = await getCity();
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=fd185eec875c277bc16bccdb3629b6af`);
+      const data = await response.json();
+      console.log(data.weather[0].icon)
+      return data.weather[0].icon;
+  }
+     imageGet();      
     async function showUserCity() {
-        userCity.innerText = await getCity();
-        temp.innerText =` ${await getTemperature()} °C`;
-        paragraphe.append(userCity,temp)
+        
+        userCity.innerHTML = `${await getCity()} `;
+        temp.innerHTML =` ${await getTemperature()} °C `;
+        image.src = `http://openweathermap.org/img/wn/${await imageGet()}@2x.png`;
+        paragraphe.append(userCity,temp,image)
         console.log(userCity,temp)
     }
-    showUserCity()
-
-    
-   
+    showUserCity(); 
     
     
   })();
