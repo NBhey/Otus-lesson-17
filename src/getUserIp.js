c8a4c3be5231fac05f2e945dfa072246827054ff
Oganes <og.marckaryan@yandex.ru>
2024-04-10 09:54:24 +0300

    add gh-pages

--- src/getUserIp.js
+++ src/getUserIp.js
@@ -3,5 +3,5 @@ export default async function getCity() {
       `https://get.geojs.io/v1/ip/geo.json`
     );
     const data = await response.json();
-    return data.city;
+    return data;
   }
--- src/index.js
+++ src/index.js
@@ -1,110 +1,116 @@
-import getCity from './getUserIp'
-import saveList from './localStorage'
-import { getItem } from './localStorage'
-import { countCity } from './countCity'
-import "./style/style.css"
+import getCity from "./getUserIp";
+import saveList from "./localStorage";
+import { getItem } from "./localStorage";
+import { countCity } from "./countCity";
+import "./style/style.css";
 
+const API_KEY = "fd185eec875c277bc16bccdb3629b6af";
 
-const API_KEY = 'fd185eec875c277bc16bccdb3629b6af'
+const titleCity = document.querySelector(".title-city");
 
-const titleCity = document.querySelector('.title-city');
+const city = document.createElement("p");
+city.classList.add("city");
 
-const city = document.createElement('p');
-city.classList.add('city');
+const temperature = document.createElement("p");
+temperature.classList.add("temperature");
 
-const temperature = document.createElement('p');
-temperature.classList.add('temperature');
+const image = document.createElement("img");
+image.classList.add("imageUser");
 
-const image = document.createElement('img')
-    image.classList.add('imageUser');
-
-export async function global() {
-const userCity = async function() {
+async function global() {
+  const userCity = async function () {
     const result = await getCity();
-    return result;
-}
-
- async function showTemperature(currentCity){
+    return result.city;
+  };
+  async function getCoordinate(){
+    const data = await getCity();
+    const coorArr = [];
+    coorArr.push(data.longitude,data.latitude);
+    return coorArr;
+  }
+  async function showTemperature(currentCity) {
     const dataCity = await currentCity;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=${API_KEY}`);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=${API_KEY}`
+    );
     const data = await response.json();
-    return Math.round(data.main.temp)
-}
-async function imageGet(currentCity){
+    return Math.round(data.main.temp);
+  }
+  async function imageGet(currentCity) {
     const dataCity = await currentCity;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=${API_KEY}`);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${dataCity}&appid=${API_KEY}`
+    );
     const data = await response.json();
     return data.weather[0].icon;
   }
- async function showUserCity(currentCity) {
+  async function showUserCity(currentCity) {
     city.innerText = await currentCity;
-    temperature.innerText =` ${await showTemperature(currentCity)} °C `;
-    image.src = `http://openweathermap.org/img/wn/${await imageGet(currentCity)}@2x.png`;
-    return titleCity.append(city,temperature,image)
-}
+    temperature.innerText = ` ${await showTemperature(currentCity)} °C `;
+    image.src = `http://openweathermap.org/img/wn/${await imageGet(
+      currentCity
+    )}@2x.png`;
+    return titleCity.append(city, temperature, image);
+  }
+  
+  showUserCity(userCity());
 
-showUserCity(userCity()); 
+  initMap();
 
- function drawList(el,input) {
-    el.innerHTML = input.map((el) => `<p>${el}</p>`).join('');
+  async function initMap() {
+    await ymaps3.ready;
+
+    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+    const map = new YMap(document.getElementById("map"), {
+      location: {
+        center: await getCoordinate(),
+        zoom: 10,
+      },
+    });
+
+    map.addChild(new YMapDefaultSchemeLayer());
+  }
+
+  function drawList(el, input) {
+    el.innerHTML = input.map((el) => `<p>${el}</p>`).join("");
     console.log(el);
     console.log(input);
-    for (let i = 0; i < el.children.length; i++){
-       console.log(el.children[i].textContent);
-       el.children[i].addEventListener('click', (e) => {
+    for (let i = 0; i < el.children.length; i++) {
+      console.log(el.children[i].textContent);
+      el.children[i].addEventListener("click", (e) => {
         e.preventDefault();
-        showUserCity(el.children[i].textContent)
-       });
+        showUserCity(el.children[i].textContent);
+      });
     }
     console.log(el);
     console.log(input);
-    if (input.length === 5){
-       input.splice(0,1);
+    if (input.length === 5) {
+      input.splice(0, 1);
     }
- }
+  }
 
-const input = document.querySelector('.input-city');
-const button = document.querySelector('.btn');
-const listEl = document.querySelector("#list");
+  const input = document.querySelector(".input-city");
+  const button = document.querySelector(".btn");
+  const listEl = document.querySelector("#list");
 
-const readList = await getItem();
+  const readList = await getItem();
 
-drawList(listEl,readList);
-button.addEventListener('click', () => {
+  drawList(listEl, readList);
+  button.addEventListener("click", () => {
     let value = input.value.trim();
-    if (value === '') {alert('Введите значение в поле')}
+    if (value === "") {
+      alert("Введите значение в поле");
+    }
     showUserCity(value);
     readList.push(value);
-    drawList(listEl,readList);
+    drawList(listEl, readList);
     saveList(readList);
     countCity(listEl);
-    input.value = ''
-});
-
+    input.value = "";
+  });
 }
 global();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // С лекции подсказка
 
